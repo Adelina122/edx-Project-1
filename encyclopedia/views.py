c2f9799ead39d0d4fc31bce85ed8a3aa2ee6eda9
Аделина Винник-Крупчан <adavinnik@gmail.com>
2023-06-24 22:03:40 +0200

    Add Entry Page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-
 from . import util
-
+from markdown2 import markdown
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def title(request, title):
+    content = util.get_entry(title)
+    if content is None:
+        content = "##Entry not found"
+
+    content = markdown(content)
+    return render(request, "encyclopedia/entry.html", {
+        "title" : title, "content" : content
     })
 
