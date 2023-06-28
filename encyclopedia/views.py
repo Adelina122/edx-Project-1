b33ef656e74586e6da3cc69edd58da53d2d10f4f
Аделина Винник-Крупчан <adavinnik@gmail.com>
2023-06-28 23:13:34 +0200

    Reverting to previous commit

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponseRedirect
+from django import forms
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from . import util
 from markdown2 import markdown
+
+class NewPageForm(forms.Form):
+    pageName = forms.CharField(widget=forms.TextInput(attrs={'label': 'Title', 'placeholder': 'Enter a title for the page'}))
+    pageContent = forms.CharField(widget=forms.Textarea(attrs={'name': 'Content', 'placeholder': 'Enter a content for the page', 'rows': '3', 'cols': '5'}))
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -26,4 +31,19 @@ def search(request):
     return render(request, "encyclopedia/search.html", {"results": util.search(q), "q": q})
 
 def newpage(request):
-    return render(request, "encyclopedia/newpage.html")
+    if request.method == "POST":
+        form = NewPageForm(request.POST)
+        if form.is_valid():
+            pageName = form.cleaned_data['pageName']
+            pageContent = form.cleaned_data['pageContent']
+            if pageName in util.list_entries():
+                return render(request, "encyclopedia/entry.html", {"message": "Title already exist. Try another.", "title": pageName, "content": pageContent})
+            util.save_entry(pageName, pageContent)
+            return redirect("encyclopedia:title", title=pageName)
+        else:
+            return render(request, "encyclopedia/newpage.html", {
+                "form" : form
+            })
+    return render(request, "encyclopedia/newpage.html", {
+        "form" : NewPageForm()
+    })
